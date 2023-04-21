7a2a73377a9fd08a896eb7ca77d04041468db1e6
Daryl Bacurin <darylbacurin14@gmail.com>
2023-04-21 17:28:38 +0800

    New app.js code structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import Login from './components/pages/Login';
@@ -8,14 +8,24 @@ import './styles/global.css'
 function App() {
   return (
     <Router>
-      <Header />
+      <MainContent />
+    </Router>
+  );
+}
+
+function MainContent() {
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname !== '/login' && <Header />} {/* Render the header only if the URL path is not "/login" */}
       <main>
         <Routes>
           <Route path="/login" element={<Login />} />
         </Routes>
       </main>
-      <Footer />
-    </Router>
+      {location.pathname !== '/login' && <Footer />} {/* Render the footer only if the URL path is not "/login" */}
+    </>
   );
 }
 
